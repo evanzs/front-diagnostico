ed80a8de31833429cdd419689e8ab595348e59e8
evanzs <evandrofidk@hotmail.com>
2023-05-18 08:20:09 -0300

    feat(question)::sparkles: add questions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
-import { QuestionsComponent } from './questions/questions.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { QuestionsComponent } from './questions/questions.component';
+import {MatExpansionModule} from '@angular/material/expansion'; 
+import {MatInputModule} from '@angular/material/input'; 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
+    MatSlideToggleModule,
+    NgbModule,
+    NgbAccordionModule,
+    MatExpansionModule,
+    MatInputModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+interface Question {
+  id: string;
+  text: string;
+  rating: number;
+  description:string;
 
+
+}
+
+interface Section {
+  id: string;
+  name: string;
+  description:string;
+  questions: Question[];
+  endRate:number;
+}
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
-export class QuestionsComponent {
 
+
+export class QuestionsComponent{
+  constructor(){}
+  panelOpenState = false;
+  sections: Section[] = [{
+    id: "1",
+    name: "Section 1",
+    description:"section about nothing",
+    endRate:10,
+    questions: [
+      {
+        id: "1",
+        text: "How would you rate the customer service?",
+        rating: 4,
+        description:"would rate",
+
+      },
+      {
+        id: "2",
+        text: "What is your opinion about the product?",
+        rating: 3,
+        description:"would rate",
+
+      },
+    ],
+  }]
 }
