727a21084ad5213058eb2a6117ae258ec1f64c2a
evanzs <evandrofidk@hotmail.com>
2023-05-18 09:44:36 -0300

    feat(login.component)::sparkles: add navigate router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatButtonModule} from '@angular/material/button'; 
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     AppRoutingModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +9,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) { }
 
   formLogin = new FormGroup({
     password: new FormControl('',Validators.required),
-    user: new FormControl('',[Validators.required,Validators.email])
+    email: new FormControl('',[Validators.required,Validators.email])
   })
 
 
-  applyLogin(){
+  submitLogin(){
+ 
     if(this.formLogin.invalid)
       return;
 
-    
+    this.router.navigate(['/questions'])
   }
 }
