41b4ccc3c482d733cd9de75308cd6584b81cbd1a
evanzs <evandrofidk@hotmail.com>
2023-05-30 18:42:52 -0300

    refactor(question)::hammer: refactoring interface

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,20 +1,6 @@
 import { Component } from '@angular/core';
-interface Question {
-  id: string;
-  text: string;
-  rating: number;
-  description:string;
+import { Section } from '../models/section';
 
-
-}
-
-interface Section {
-  id: string;
-  name: string;
-  description:string;
-  questions: Question[];
-  endRate:number;
-}
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
@@ -26,20 +12,20 @@ export class QuestionsComponent{
   constructor(){}
   panelOpenState = false;
   sections: Section[] = [{
-    id: "1",
+
     name: "Section 1",
     description:"section about nothing",
     endRate:10,
     questions: [
       {
-        id: "1",
+    
         text: "How would you rate the customer service?",
         rating: 4,
         description:"would rate",
 
       },
       {
-        id: "2",
+   
         text: "What is your opinion about the product?",
         rating: 3,
         description:"would rate",
