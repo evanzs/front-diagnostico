2d8fdfaebdf3b48ceee456e8b828d65067677f31
evanzs <evandrofidk@hotmail.com>
2023-06-02 00:36:55 -0300

    refactor(menu.ts)::hammer: add user info

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserApp } from '../models/userApp';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   isMenuOpen= false;
+
+  @Input() user!:UserApp;
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
