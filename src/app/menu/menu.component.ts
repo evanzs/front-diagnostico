d668846d6abbfdc9e8403928ef218e8db479358b
evanzs <evandrofidk@hotmail.com>
2023-06-07 17:08:17 -0300

    reafactor(menu.component.ts)::hammer:  add projectservice and authService to component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserApp } from '../models/userApp';
+import { Project } from '../models/project';
+import { ProjectService } from '../project.service';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
-  isMenuOpen= false;
+export class MenuComponent implements OnInit{
 
   @Input() user!:UserApp;
+
+  constructor(
+    private readonly _projectService:ProjectService,
+    private readonly _loginService:LoginService,
+    private readonly _authService:AuthService,
+    ){
+    
+  }
+
+  project!:Project;
+  isMenuOpen= false;
+  enableMenuBar = false;
+  enableMenuNav = false;
+  ngOnInit(): void {
+     this._projectService.emitEventoProject.subscribe( (project) => this.project = project)
+     this._authService.emitirUserApp.subscribe( (user) => this.user = user)
+
+     this._loginService.emitEventoEnableMenuBar.subscribe( (enableMenuBar) => this.enableMenuBar = enableMenuBar)
+     this._projectService.emitEventoEnableMenuNav.subscribe( (enableMenuNav) => this.enableMenuNav = enableMenuNav)
+
+  }
+
+
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
