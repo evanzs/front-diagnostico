437aeb57819d7d445520ec0bbc99e0a0ebbdbd88
evanzs <evandrofidk@hotmail.com>
2023-06-21 22:04:25 -0300

    refactor(question.component):hammer:  add observable project infos

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,7 +1,12 @@
 import { UserApp } from './../models/userApp';
 import { Component, OnInit } from '@angular/core';
-import { Section } from '../models/section';
+
 import { AuthService } from '../auth.service';
+import { ProjectService } from '../project.service';
+import { Principle } from '../models/principle';
+import { Project } from '../models/project';
+import { Observable, map, switchMap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-questions',
@@ -13,31 +18,33 @@ import { AuthService } from '../auth.service';
 export class QuestionsComponent implements OnInit{
 
   user!:UserApp;
-  constructor(private readonly _authService:AuthService){}
+  id!:string;
+  constructor(private readonly _authService:AuthService,
+    private readonly _projectService:ProjectService,
+    private readonly _route:ActivatedRoute){}
+    project!:Project;
+    principle!:Principle;
   ngOnInit(): void {
     this._authService.emitirUserApp.subscribe( user => this.user = user)
+
+    this._route.params 
+    .subscribe(params => {
+      this.project = this._projectService.getEnvProject()
+      this.setPrinciple(params['id'])
+     
+    })
+ 
   }
   panelOpenState = false;
-  sections: Section[] = [{
+  teste(teste:any){
+  
+    this.panelOpenState = false;
+  }
 
-    name: "Section 1",
-    description:"section about nothing",
-    endRate:10,
-    questions: [
-      {
-    
-        text: "How would you rate the customer service?",
-        rating: 4,
-        description:"would rate",
-
-      },
-      {
-   
-        text: "What is your opinion about the product?",
-        rating: 3,
-        description:"would rate",
-
-      },
-    ],
-  }]
+  setPrinciple(id:string){
+    const result = this.project.principles.find( p => p._id === id)
+    if(result)
+      this.principle =result
+      
+  }
 }
