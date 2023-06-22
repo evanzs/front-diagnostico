bd7133fd4df75d901e0f6c065445fc9fdf03b735
evanzs <evandrofidk@hotmail.com>
2023-06-21 22:04:44 -0300

    refactor(menu.component):hammer:  add observable project infos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,6 +4,8 @@ import { UserApp } from '../models/userApp';
 import { Project } from '../models/project';
 import { ProjectService } from '../project.service';
 import { LoginService } from '../login/login.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -18,20 +20,22 @@ export class MenuComponent implements OnInit{
     private readonly _projectService:ProjectService,
     private readonly _loginService:LoginService,
     private readonly _authService:AuthService,
+    private readonly _router:Router,
+    private readonly _route:ActivatedRoute
     ){
     
   }
 
-  project!:Project;
+  project$!:Observable<Project>;
   isMenuOpen= false;
   enableMenuBar = false;
   enableMenuNav = false;
   ngOnInit(): void {
-     this._projectService.emitEventoProject.subscribe( (project) => this.project = project)
+     this.project$ = this._projectService.getSelectedProject();
      this._authService.emitirUserApp.subscribe( (user) => this.user = user)
 
      this._loginService.emitEventoEnableMenuBar.subscribe( (enableMenuBar) => this.enableMenuBar = enableMenuBar)
-     this._projectService.emitEventoEnableMenuNav.subscribe( (enableMenuNav) => this.enableMenuNav = enableMenuNav)
+     this._loginService.emitEventoEnableMenuNav.subscribe( (enableMenuNav) => this.enableMenuNav = enableMenuNav)
 
   }
 
@@ -39,5 +43,9 @@ export class MenuComponent implements OnInit{
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  openPrinciple(id:string){
+    this._router.navigate(['/questions',id])
   }
 }
