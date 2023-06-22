8c151d1ec958d0aa14a28f185ca0e895f30506cc
evanzs <evandrofidk@hotmail.com>
2023-06-22 20:33:17 -0300

    refactor(auth.guard.ts)::hammer:   enable nav e bar if no expired

--- src/app/guards/auth.guard..ts
+++ src/app/guards/auth.guard..ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { Observable } from 'rxjs';
+import { LoginService } from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { Observable } from 'rxjs';
 export class AuthGuard {
 
  constructor(private readonly _authService:AuthService,
-  private readonly router:Router) { }
+  private readonly router:Router,private readonly _loginService:LoginService) { }
 
   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot):boolean | Promise<boolean> | Observable<boolean>{
      const isTokenExpired  = this._authService.isTokenExpired();
@@ -19,7 +20,8 @@ export class AuthGuard {
         this.router.navigate(['login'])
         return false;
      }
-
+     this._loginService.enableMenuBar();
+     this._loginService.enableMenuNav();
      return true;       
   }
 
