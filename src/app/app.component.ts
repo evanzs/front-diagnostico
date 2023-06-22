a13f540b57f2367a9e44bb1a3659dd0c80ffa4c2
evanzs <evandrofidk@hotmail.com>
2023-06-21 21:57:19 -0300

    refactor(app.component):hammer: remove unsage function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,15 @@
 import { AppService } from './app.service';
 import { Component, OnInit } from '@angular/core';
-import { Section } from './models/section';
+import { Principle } from './models/principle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor( public appService:AppService){
+  constructor(){
 
   }
-  ngOnInit(): void {
-    console.log("inicial")
-    this.emitirValor();
-  }
-
-  emitirValor(){
-    console.log("emitiu",this.sections)
-  this.appService.emitValor(this.sections)
-  }
-  sections: Section[] = [{
-
-    name: "Section 1",
-    description:"section about nothing",
-    endRate:10,
-    questions: [
-      {
-    
-        text: "How would you rate the customer service?",
-        rating: 4,
-        description:"would rate",
-
-      },
-      {
-   
-        text: "What is your opinion about the product?",
-        rating: 3,
-        description:"would rate",
-
-      },
-    ],
-  }]
 
 }
