bd6de8cf892a6c12ed6b662e6dac6b4b840c1846
evanzs <evandrofidk@hotmail.com>
2023-07-05 20:54:47 -0400

    refactor(login.component)::hammer: change responses menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
 import { AuthService } from '../auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateProjectComponent } from '../create-project/create-project.component';
+import { KeyDailogComponent } from '../key-dailog/key-dailog.component';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,10 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
 
-  constructor(private router: Router,private readonly _service:LoginService, private readonly _authService:AuthService) { }
+  constructor(private router: Router,
+    private readonly _service:LoginService, 
+    private readonly _authService:AuthService,
+    public dialog: MatDialog) { }
 
   formLogin = new FormGroup({
     password: new FormControl('',Validators.required),
@@ -25,7 +31,9 @@ export class LoginComponent {
       return;
     const {email,password} =this.formLogin.value;
       this._service.logIn({email,password}).subscribe({ next: (res)=>{  
-      this._authService.setToken(res.access_token)
+      this._authService.setToken(res.access_token)      
+      this._authService.getUserToken()      
+
       this._service.enableMenuNav()
       this._service.enableMenuBar()
       this.router.navigate(['/home'])
@@ -36,5 +44,9 @@ export class LoginComponent {
 
   register(){
     this.router.navigate(['/register'])
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(KeyDailogComponent);
   }
 }
