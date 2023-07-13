9a9bdd27c987a4635e00fab95a4b3f04f21bb5d8
evanzs <evandrofidk@hotmail.com>
2023-07-13 10:35:47 -0300

    refactor(responses.component)::hammer:  add service to request responses

--- src/app/responses/responses.component.ts
+++ src/app/responses/responses.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { Project } from '../models/project';
 import { ProjectService } from '../project.service';
+import { ResponseQuestion } from '../models/response-question';
 
 @Component({
   selector: 'app-responses',
@@ -13,6 +14,7 @@ import { ProjectService } from '../project.service';
 export class ResponsesComponent implements OnInit {
 
   id!:string;
+  responseQuestion!:ResponseQuestion[]
 
   constructor(private readonly _authService:AuthService,
     private readonly _projectService:ProjectService,
@@ -21,16 +23,33 @@ export class ResponsesComponent implements OnInit {
     project!:Project;
     indexPrinciple:number = 0;
 
-  displayedColumns: string[] = ['email', 'createdAt','acoes'];
+  displayedColumns: string[] = ['email', 'createdAt','completed','acoes'];
   data =[
-   { email:"evandro@com.br",createdAt:"2022-02-05"},
-   { email:"evandro@com.br",createdAt:"2022-02-15"},
-   { email:"evandro@com.br",createdAt:"2022-02-08"},
-   { email:"Resposta Anonima",createdAt:"2022-02-08"},
+   { email:"evandro@com.br",completed:true,createdAt:"2022-02-05"},
+   { email:"evandro@com.br",completed:true,createdAt:"2022-02-15"},
+   { email:"evandro@com.br",completed:true,createdAt:"2022-02-08"},
+   { email:"Resposta Anonima",completed:true,createdAt:"2022-02-08"},
   ]
   ngOnInit(): void {
-
+    this.project = this._projectService.getEnvProject();
+    this.getResponseByProjectId();
     
+  }
+
+
+  getResponseByProjectId(){
+    if(!this.project)
+    return;
+
+   this._projectService.getResponsesByProjectId(this.project._id).subscribe({
+    next:(res) => {
+      this.responseQuestion = res;
+    },
+    error:() =>{
+      this._router.navigate(['home'])
+    }
+   })
+  
   }
   
   deleteResponse(project:Project):void{}
