faff7b0dcee5d29878836eebd81040e346186aec
evanzs <evandrofidk@hotmail.com>
2023-07-13 10:34:51 -0300

    refactor(result.component)::hammer: refactor component  to provide data

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,82 +1,127 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 
 import { radarChartOptions } from '../models/radarOptions';
+import { ResultService } from './result.service';
+import { ProjectService } from '../project.service';
+import { Project } from '../models/project';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit {
+
+  project!:Project;
+  constructor(private _resultService:ResultService,
+    private _projectservice:ProjectService,
+    private _router:Router,
+    private readonly _route:ActivatedRoute
+    ){ 
+
+  }
+
+  radarChartData!: ChartData<'radar'>;
+
+  public radarChartOptions: ChartConfiguration['options'] = radarChartOptions;
+  public radarChartType: ChartType = 'radar';
+  labels!:string[];
+  principlesNames!:string[];
+  result!:any;
+
+  selectedPrinciple = "Congruência";
+  loading = false;
+  ngOnInit(): void {
+
+    this._route.params 
+    .subscribe({
+      next:(params) =>{          
+        this.selectedPrinciple = params['name']
+        this.project = this._projectservice.getEnvProject();
+        this._resultService.getResultByProjectId(this.project?._id).subscribe((res) =>{
+          this.result = res;
+          this.transformData(res)
+          this.radarChartData = this.buildData();
+        })
+      }
+    })
+  }
+
+
+  transformData(result:any):void{
+    const resultCreator = result.find((r:any) => r.creator === true)
+
+    if(!resultCreator)
+        return;
+    
+    const {results} = resultCreator;
+
+    if(!results)
+        return;
+    
+    let  data = results.filter((r:any) => r.principle === this.selectedPrinciple);
+
+    let principle = results.map(({ principle }:any) => principle)
+    const labels = data.map(({ guideline }:any) => guideline);    
+    const dataset = data.map(({ rate }:any) => {
+      if(!rate)
+          return 0
+     return rate
+    });   
+    if(labels.length> 0 && dataset.length> 0 ){
+
+      this.setLabels(labels);
+      this.setDataset(dataset)
+  
+    }         
+
+    if(principle.length > 0){
+        principle = [... new Set(principle)]
+       this.principlesNames =     principle ;
+    }
+    
+  
+
+  }
+  setLabels(labels:string[])   {
+
+    this.labels=labels;
+    this.radarChartLabels =labels
+  }
+  setDataset(dataset:number[]){
+   
+    this.dataset = dataset
+    
+  }
  // Radar
- public radarChartOptions: ChartConfiguration['options'] = radarChartOptions;
+radarChartLabels: string[] =[]
+dataset:Array<number | null>= []
 
-public radarChartLabels: string[] = 
-['Conteúdo e fonte alimentar culturalmente apropriados',
-'Maximização da utilização de alimentos',
-'Respeito aos direitos',
-'Respeito aos limites biofísicos',
-'Sinergia ecológica',
-'Tecnologias apropriadas a escala, cultura e economia'];
-
-
-public radarChartData: ChartData<'radar'> = {
-  labels: this.radarChartLabels,
+buildData(){
+  return {
+    labels: this.radarChartLabels,
+    
+    datasets: [
+      { data: this.dataset,
+      label:'CONGRUÊNCIA'},
+    ],
   
-  datasets: [
-    { data: [1.74025974,
-      1.448148148,
-      0.3555555556,
-      1.144671202,
-      1.536111111,
-      1.766666667],
-    label:'CONGRUÊNCIA'},
-  ],
+    
+  };
+}
+
+changeSelect(item:string){
+
+
+    this.selectedPrinciple = item;
+    
+    if(this.result.length > 0){  
+       this.transformData(this.result)
+    }
 
   
-};
-public radarChartType: ChartType = 'radar';
-
-// events
-public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
-  console.log(event, active);
 }
-
-public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
-  console.log(event, active);
-}
-
-export(){
-  const chartCanvas = document.getElementById('chart') as HTMLCanvasElement;
-  const canvas = document.createElement('canvas'); // Crie um canvas temporário
-  if(!canvas) return;
-  canvas.width = chartCanvas.width;
-  canvas.height = chartCanvas.height;
-  const ctx = canvas.getContext('2d');
-  if(!ctx) return;
-  // Preencha o canvas com um fundo branco
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Renderize o gráfico no canvas temporário
-  ctx.drawImage(chartCanvas, 0, 0);
-
-  // Converta o canvas para uma imagem PNG
-  canvas.toBlob((blob) => {
-
-    if(!blob) return;
-    const url = URL.createObjectURL(blob);
-
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'chart.png';
-    link.click();
-
-    // Clean up the URL object
-    URL.revokeObjectURL(url);
-  });
-}
-
 }
  
 
