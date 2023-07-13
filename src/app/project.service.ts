375e4c6d092c9293fb78e0b4d635e33d33a01267
evanzs <evandrofidk@hotmail.com>
2023-07-13 10:28:36 -0300

    refactor(project.service)::hammer: add response env and observable

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -3,6 +3,7 @@ import { Project } from './models/project';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ResponseQuestion } from './models/response-question';
 
 @Injectable()
 export class ProjectService {
@@ -10,8 +11,9 @@ export class ProjectService {
 private readonly URL = environment.apiUrl
 
 private project!:Project;
+private responseQuestion!:ResponseQuestion;
 private selectedProject  = new Subject<Project>();
-
+private selectedResponse  = new Subject<ResponseQuestion>();
 constructor(private http: HttpClient) { }
 
 
@@ -20,16 +22,60 @@ constructor(private http: HttpClient) { }
         this.setEnvProject(project)      
     }
 
+    loadResponse(responseQuestion:ResponseQuestion){
+        this.setResponse(responseQuestion) 
+        this.setEnvResponse(responseQuestion)      
+    }
+
     setProject(project:Project){
         
         this.selectedProject.next(project);
     }
+
+    setResponse(responseQuestion:ResponseQuestion){
+        
+        this.selectedResponse.next(responseQuestion);
+    }
     getSelectedProject():Observable<Project> {
         return this.selectedProject.asObservable();      
+    }
+    getSelectedResponse():Observable<ResponseQuestion> {
+        return this.selectedResponse.asObservable();      
+    }
+
+    setEnvProject(project:Project):void{
+        this.project = project
+    }
+
+    setEnvResponse(responseQuestion:ResponseQuestion):void{
+        this.responseQuestion = responseQuestion
+    }
+
+
+    getEnvProject():Project{
+        return this.project
+    }
+
+    getEnvResponse():ResponseQuestion{
+        return this.responseQuestion
     }
   
     getProjects(userId:string):Observable<any>{
         return this.http.get<any>(this.URL+"/project/user/"+userId)       
+    }
+
+    getResponseById(responseId:string):Observable<any>{
+        return this.http.get<any>(this.URL+"/project/response/"+responseId)
+    }
+
+
+    getResponsesByProjectId(responseId:string):Observable<ResponseQuestion[]>{
+        return this.http.get<any>(this.URL+"/project/"+responseId+"/response")
+    }
+
+
+    getProjectById(projectId:string):Observable<any>{
+        return this.http.get<any>(this.URL+"/project/"+projectId)
     }
     createProject(userId:string,data:any):Observable<any>{
         return this.http.post<any>(this.URL+"/project/"+userId,data)       
@@ -39,13 +85,18 @@ constructor(private http: HttpClient) { }
         return this.http.patch<any>(this.URL+"/project/"+projectId,data)       
     }
 
-    setEnvProject(project:Project):void{
-        this.project = project
+    updateCreatorResponse(projectId:string,data:any):Observable<any>{
+        return this.http.patch<any>(this.URL+"/project/"+projectId+"/creator",data)       
     }
 
-    getEnvProject():Project{
-        return this.project
+    createResponse(projectId:string,data:any):Observable<any>{
+        return this.http.post<any>(this.URL+"/project/"+projectId+"/response",data)       
     }
+    
+    updateResponse(responseId:string,data:any):Observable<any>{
+        return this.http.patch<any>(this.URL+"/project/response/"+responseId,data)       
+    }
+
 
     deleteProject(id:string):Observable<any>{        
         return this.http.delete<any>(this.URL+"/project/"+id)   
