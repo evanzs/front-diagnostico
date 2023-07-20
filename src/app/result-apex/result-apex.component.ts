71426195c5f8873126df3b5606232864c6a6aae4
evanzs <evandrofidk@hotmail.com>
2023-07-19 21:14:47 -0300

    feat(result-filter)::sparkles:  add project

--- src/app/result-apex/result-apex.component.ts
+++ src/app/result-apex/result-apex.component.ts
@@ -11,6 +11,7 @@ export class ResultApexComponent implements OnChanges {
 
   @Input() radarChartData!: ChartData<'radar'>;
   data!:ChartData<'radar'>;
+  @Input() exportCanvas = false;
   load = false;
   public radarChartType!: ChartType;
     public radarChartOptions: ChartConfiguration['options'];
@@ -21,8 +22,42 @@ export class ResultApexComponent implements OnChanges {
       this.radarChartOptions= radarChartOptions;
        
       this.data = this.radarChartData;
-
       this.load=true;
+
+      if(this.exportCanvas)
+        this.export();
     }
+
+    export(){
+      const chartCanvas = document.getElementById('chart') as HTMLCanvasElement;
+      const canvas = document.createElement('canvas'); // Crie um canvas temporário
+      if(!canvas) return;
+      canvas.width = chartCanvas.width;
+      canvas.height = chartCanvas.height;
+      const ctx = canvas.getContext('2d');
+      if(!ctx) return;
+      // Preencha o canvas com um fundo branco
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+      // Renderize o gráfico no canvas temporário
+      ctx.drawImage(chartCanvas, 0, 0);
+    
+      // Converta o canvas para uma imagem PNG
+      canvas.toBlob((blob) => {
+    
+        if(!blob) return;
+        const url = URL.createObjectURL(blob);
+    
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'chart.png';
+        link.click();
+    
+        // Clean up the URL object
+        URL.revokeObjectURL(url);
+      });
+    }
+    
   
 }
