dd7b2edfb8431ad18d85d38b04aaa259befe4379
evanzs <evandrofidk@hotmail.com>
2023-07-19 21:14:32 -0300

    feat(result-filter)::sparkles:  add project

--- src/app/result-filter/result-filter.component.ts
+++ src/app/result-filter/result-filter.component.ts
@@ -18,32 +18,44 @@ export class ResultFilterComponent implements OnInit {
     private _router:Router,
     private readonly _route:ActivatedRoute
     ){ 
-
   }
 
   radarChartData!: ChartData<'radar'>;
 
-
+  exportCanvas = false;
   labels!:string[];
   principlesNames!:string[];
   result!:any;
 
   selectedPrinciple = "Congruência";
   loading = false;
+
+      // Radar
+      radarChartLabels: string[] =[]
+      datasetResponser:Array<number | null>= []
+      datasetCreator:Array<number | null>= []
+
+  
   ngOnInit(): void {
         this.project = this._projectservice.getEnvProject();
+        if(this.project){
+          this._projectservice.loadProject(this.project)
+        }
         this._resultService.getResultByProjectId(this.project?._id).subscribe((res) =>{
           this.result = res;
-          this.transformData(res)
-          this.radarChartData = this.buildData();
+          this.transformDataCreator(res)
+          this.transformDataResponses(res)
+          this.radarChartData = this.buildData()
         })     
+
+        this.exportCanvas = false;
 
   }
 
 
-  transformData(result:any):void{
+  transformDataCreator(result:any):void{
     const resultCreator = result.find((r:any) => r.creator === true)
-
+    
     if(!resultCreator)
         return;
     
@@ -64,39 +76,83 @@ export class ResultFilterComponent implements OnInit {
     if(labels.length> 0 && dataset.length> 0 ){
 
       this.setLabels(labels);
-      this.setDataset(dataset)
+      this.setDatasetCreator(dataset)
   
     }         
 
     if(principle.length > 0){
         principle = [... new Set(principle)]
        this.principlesNames =     principle ;
-    }
-    
-  
+    } 
 
   }
-  setLabels(labels:string[])   {
 
+
+  transformDataResponses(result:any):void{
+    const resultCreator = result.find((r:any) => r.creator === false)
+
+    if(!resultCreator)
+        return;
+    
+    const {results} = resultCreator;
+
+    if(!results)
+        return;
+    
+    let  data = results.filter((r:any) => r.principle === this.selectedPrinciple);
+    const dataset = data.map(({ rate }:any) => {
+      if(!rate)
+          return 0
+     return rate
+    });   
+    if(dataset.length> 0 ){
+  
+      this.setDatasetResponser(dataset)  
+    }  
+  }
+
+  setLabels(labels:string[])   {
     this.labels=labels;
     this.radarChartLabels =labels
   }
-  setDataset(dataset:number[]){
-   
-    this.dataset = dataset
-    
+
+  setDatasetCreator(dataset:number[]){    
+    this.datasetCreator = dataset;
   }
- // Radar
-radarChartLabels: string[] =[]
-dataset:Array<number | null>= []
+
+  setDatasetResponser(dataset:number[]){    
+    this.datasetResponser = dataset;
+  }
+
+  setExport(){
+    this.exportCanvas = true;
+  }
 
 buildData(){
   return {
     labels: this.radarChartLabels,
     
     datasets: [
-      { data: this.dataset,
-      label:'CONGRUÊNCIA'},
+      { data: this.datasetCreator,
+      label:'Criador',
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
+    },
+    { data: this.datasetResponser,
+      label:'Participantes',
+      fill: true,
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgb(54, 162, 235)',
+      pointBackgroundColor: 'rgb(54, 162, 235)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(54, 162, 235)'
+    },
     ],
   
     
@@ -104,15 +160,16 @@ buildData(){
 }
 
 changeSelect(item:string){
-  this.radarChartData.datasets = []
-  this.loading = true;
+
+
     this.selectedPrinciple = item;
     
-    if(this.result.length > 0){  
-       this.transformData(this.result)
+    if(this.result.length > 0){ 
+       this.transformDataCreator(this.result)
+       this.transformDataResponses(this.result)
        this.radarChartData = this.buildData();
     }
 
-  this.loading =false
+  
 }
 }
