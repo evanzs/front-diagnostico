f0770869e80e0519eafa8b9824a052d7a821aeb4
evanzs <evandrofidk@hotmail.com>
2023-07-19 21:13:54 -0300

    chore(enviroment)::upload environtment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+     //apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://api-diagnostico-yprmvjwzqq-uc.a.run.app',
     // Adicione outras variáveis de ambiente aqui
   };
   
