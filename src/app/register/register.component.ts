4a7d491a09f5db8e9a1374fab7ba67d677ef4bb9
evanzs <evandrofidk@hotmail.com>
2023-07-19 21:18:15 -0300

    refator(register)::hammer: add feedback snackbar and modal

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginService } from '../login/login.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private _snackBar: MatSnackBar,private router:Router,  private formBuilder: FormBuilder,private _loginService:LoginService) {
+  constructor(private _snackBar: MatSnackBar,
+    private router:Router,  
+    private formBuilder: FormBuilder,
+    private _loginService:LoginService,
+    public dialogRef: MatDialogRef<RegisterComponent>,
+
+    ) {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       name: ['', Validators.required],
@@ -24,16 +31,35 @@ export class RegisterComponent {
 
   onSubmit() {
     if (this.registerForm.valid) {
+      const isValid = this.checkPassword();
+      if(isValid){
+        this._loginService.createUser(this.registerForm.value).subscribe({
+          next:()=>{
+            this._snackBar.open("Usuário registrado com sucesso.", 'fechar',{duration:10000,panelClass: 'snackbar-success'});
+            this.dialogRef.close();
+            this.router.navigate(['login']);
+          },
+          error:()=>{
+            this._snackBar.open("Não foi possivel realizar o registro.", 'fechar',{duration:10000});
+          }
+        })
+      }else{
+        this._snackBar.open("As senhas não coincidem.", 'fechar',{duration:10000});
 
-      this._loginService.createUser(this.registerForm.value).subscribe({
-      next:()=>{
-        this.router.navigate(['login']);
-      },
-      error:()=>{
-        this._snackBar.open("Não foi possivel realizar o registro.", 'fechar');
       }
-    })
-      // Aqui você pode adicionar a  para enviar os dados do formulário para o servidor
+    }
+  }
+
+  checkPassword(){
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+  
+    if (password !== confirmPassword) {
+      this.registerForm.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+      return false;
+    } else {
+      this.registerForm.get('confirmPassword')?.setErrors(null);
+      return true;
     }
   }
 }
