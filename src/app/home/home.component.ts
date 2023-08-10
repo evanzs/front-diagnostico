292b1328357557a24fb516b89bb82bf4deec6dd0
evanzs <evandrofidk@hotmail.com>
2023-08-10 12:28:52 -0300

    feat(home.component)::sparkles: add loading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,26 +10,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -40,6 +20,7 @@ export class HomeComponent  implements OnInit{
   user!:UserApp;
   panelOpenState = false;
   projects$!: Observable<Project[]>;
+  loading  = false;
 
   displayedColumns: string[] = ['name', 'description','city','createdAt','updatedAt','acoes'];
   dataSource = []
@@ -51,6 +32,7 @@ export class HomeComponent  implements OnInit{
     private _snackBar: MatSnackBar){}
 
   ngOnInit(): void {
+    this.loading = true;
     this.user = this._authService.getUser();     
     this.getProjects(this.user.id)  
   }
@@ -63,8 +45,10 @@ export class HomeComponent  implements OnInit{
       this.router.navigate(['/questions/'+id])
   }
 
-  getProjects(userId:string):Observable<Project>{
-   return this.projects$ = this._projectService.getProjects(userId)    
+  getProjects(userId:string):Observable<Project[]>{
+    this.projects$ = this._projectService.getProjects(userId)
+    this.loading = false;
+   return this.projects$ 
   }
 
   deleteProject(project:Project){
