bfbf984e3f1505e21378cdbe3a839c3f16d32659
evanzs <evandrofidk@hotmail.com>
2023-08-18 15:26:23 -0300

    feat(tags-map)::sparkles: add tag map

---
+++ src/app/models/tags-map.ts
@@ -0,0 +1,9 @@
+export const tagsApp = [
+    {tag:["Produção, processamento e distribuição"], filters:['Produção','Distribuição','Processamento']},
+    {tag:["Processamento"], filters:['Processamento']},
+    {tag:["Produção"], filters:['Produção']},
+    {tag:["Distribuição"], filters:['Distribuição']},
+    {tag:["Processamento e distribuição"], filters:['Distribuição','Processamento']},
+    {tag:["Produção e processamento"], filters:['Produção','Processamento']},
+    {tag:["Produção e distribuição"], filters:['Produção','Distribuição']}
+]
