53d14a740c84f58609fdb77dcec03d9ceb772065
evanzs <evandrofidk@hotmail.com>
2023-08-22 23:12:50 -0300

    feat(tagOption)::sparkles: add tagOption Model to render

---
+++ src/app/models/tagOptions.ts
@@ -0,0 +1,7 @@
+import { TagOption } from "./interfaces/tag-options";
+
+export const TAG_OPTIONS :TagOption[] = [
+    { value: 'Produção', label: 'Produção', disabled: false },
+    { value: 'Distribuição', label: 'Distribuição', color: 'accent', disabled: false },
+    { value: 'Processamento', label: 'Processamento', color: 'warn', disabled: false }
+  ];
